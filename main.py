4d6c4671e10da7dada9782bbfb6c026158fa0c3d
suibaka <suibaka@hotmail.co.jp>
2020-11-24 14:45:40 +0900

    Squashed commit of the following: (16442)
    commit cbddd81051def7d2506c07b0a42a3e216530db9d
    Author: suibaka <suibaka@hotmail.co.jp>
    Date:   Tue Nov 24 14:45:03 2020 +0900
    
        Fix
    
    commit e1b10c8366dd62cb08b42f14e016a86134d04835
    Author: suibaka <suibaka@hotmail.co.jp>
    Date:   Tue Nov 24 13:24:16 2020 +0900
    
        tmp
    
    commit dc5cccda22d095425839a9fe1a8f2f3213fa0d11
    Author: suibaka <suibaka@hotmail.co.jp>
    Date:   Tue Nov 24 12:40:05 2020 +0900
    
        Impl preproc

--- main.py
+++ main.py
@@ -3,320 +3,193 @@ def qor(qc, a, b, c):
     qc.x(b)
     qc.ccx(a, b, c)
     qc.x(c)
+
+def inv_qor(qc, a, b, c):
+    qc.x(c)
+    qc.ccx(a, b, c)
     qc.x(a)
     qc.x(b)
 
+# 列と行単位の swap は結果に影響しない
+def preprocess(stars):
+    board = [[] for i in range(4)]
+    for pos in stars:
+        r, c = int(pos[0]), int(pos[1])
+        board[r].append(c)
+    board.sort(key=len)
 
-def inner_phase_oracle(qc, perm, oracle, aux):
-    # A, B, C, D
-    # A, B, C, D+C     (2 -> 3)
-    # A, B, C+B, D+C   (1 -> 2)
-    # A, B+A, C+B, D+C (0 -> 1)
-    # A, B+A, C+B, D+B (2 -> 3)
-    # A, B+A, C+A, D+B (1 -> 2)
-    # A, B+A, C+A, D+A (1 -> 3)
-    def add_and_check(i, j, out):
-        qc.cx(perm[i*2], perm[j*2])
-        qc.cx(perm[i*2+1], perm[j*2+1])
-        qor(qc, perm[j*2], perm[j*2+1], out)
-    def inv_add_and_check(i, j, out):
-        qor(qc, perm[j*2], perm[j*2+1], out)
-        qc.cx(perm[i*2+1], perm[j*2+1])
-        qc.cx(perm[i*2], perm[j*2])
+    def swap_col(i, j):
+        for r in range(4):
+            for k in range(len(board[r])):
+                if board[r][k] == i:
+                    board[r][k] = j
+                elif board[r][k] == j:
+                    board[r][k] = i
 
-    workspace = aux[0:6]
-    def check_all():
-        add_and_check(2, 3, workspace[0])
-        add_and_check(1, 2, workspace[1])
-        add_and_check(0, 1, workspace[2])
-        add_and_check(2, 3, workspace[3])
-        add_and_check(1, 2, workspace[4])
-        add_and_check(1, 3, workspace[5])
-    def inv_check_all():
-        inv_add_and_check(1, 3, workspace[5])
-        inv_add_and_check(1, 2, workspace[4])
-        inv_add_and_check(2, 3, workspace[3])
-        inv_add_and_check(0, 1, workspace[2])
-        inv_add_and_check(1, 2, workspace[1])
-        inv_add_and_check(2, 3, workspace[0])
+    if len(board[0]) != 0:
+        swap_col(0, board[0][0])
+    if len(board[1]) != 0:
+        swap_col(1, board[1][0])
 
-    check_all()
-    qc.mct(workspace, oracle, aux[6:], mode='basic')
-    inv_check_all()
+    #print(board)
+    return board
 
-def inner_diffusion(qc, perm, aux):
-    qc.h(perm)
-    qc.x(perm)
-    qc.h(perm[7])
-    qc.mct(perm[0:7], perm[7], aux, mode='basic')
-    qc.h(perm[7])
-    qc.x(perm)
-    qc.h(perm)
+# qRAM data format:
+# 0...
+# .1..
+# ..23
+# ..45
+# 星は 6 個しかないので，適切に前処理しておけば上記の部分だけ記憶するだけで十分である
 
-# create permutations by grover search
-iter_cnt1 = 2 # 2 is best
-def create_perm(qc, perm, oracle, aux):
-    for i in range(iter_cnt1):
-        inner_phase_oracle(qc, perm, oracle, aux)
-        inner_diffusion(qc, perm, aux)
-
-def inv_create_perm(qc, perm, oracle, aux):
-    for i in range(iter_cnt1):
-        inner_diffusion(qc, perm, aux)
-        inner_phase_oracle(qc, perm, oracle, aux)
-
-
-def adder2(qc, s, x, c):
-    qc.rccx(x, s[0], c)
-    qc.cx(c, s[1])
-    qc.rccx(x, s[0], c)
-    qc.cx(x, s[0])
-
-# after call, c become dirty bit
-def adder2_dirty(qc, s, x, c):
-    qc.rccx(x, s[0], c)
-    qc.cx(c, s[1])
-    #qc.rccx(x, s[0], c)
-    qc.cx(x, s[0])
-
-def inv_adder2(qc, s, x, c):
-    qc.cx(x, s[0])
-    qc.rccx(x, s[0], c)
-    qc.cx(c, s[1])
-    qc.rccx(x, s[0], c)
-
-def inv_adder2_dirty(qc, s, x, c):
-    qc.cx(x, s[0])
-    #qc.rccx(x, s[0], c)
-    qc.cx(c, s[1])
-    qc.rccx(x, s[0], c)
-
-def is_count4(qc, xs, out, dirty, aux):
-    assert len(dirty) >= 5
-    qc.x(xs[0:3])
-    qc.mct(xs[0:3], dirty[0], aux, mode='basic')
-    qc.x(dirty[0]) # xs[0:3] に少なくとも 1 が 1 つある
-    qc.x(xs[0:3])
-    s = [xs[0], dirty[1]]
-    adder2(qc, s, xs[1], dirty[2])
-    adder2(qc, s, xs[2], dirty[2])
-    adder2_dirty(qc, s, xs[3], dirty[2])
-    adder2_dirty(qc, s, xs[4], dirty[3])
-    adder2_dirty(qc, s, xs[5], dirty[4])
-    qc.x(s[0])
-    qc.x(s[1])
-    qc.mct([dirty[0], s[0], s[1]], out, aux, mode='basic')
-    qc.x(s[1])
-    qc.x(s[0])
-
-def inv_is_count4(qc, xs, out, dirty, aux):
-    assert len(dirty) >= 5
-    s = [xs[0], dirty[1]]
-    qc.x(s[0])
-    qc.x(s[1])
-    qc.mct([dirty[0], s[0], s[1]], out, aux, mode='basic')
-    qc.x(s[1])
-    qc.x(s[0])
-    inv_adder2_dirty(qc, s, xs[5], dirty[4])
-    inv_adder2_dirty(qc, s, xs[4], dirty[3])
-    inv_adder2_dirty(qc, s, xs[3], dirty[2])
-    inv_adder2(qc, s, xs[2], dirty[2])
-    inv_adder2(qc, s, xs[1], dirty[2])
-    qc.x(xs[0:3])
-    qc.x(dirty[0])
-    qc.mct(xs[0:3], dirty[0], aux, mode='basic')
-    qc.x(xs[0:3])
-
-# addr == 1
-def check_one_board(qc, addr, perm, oracle, aux, stars):
-    assert len(aux) >= 15
-    workspace = aux[0:6]
-    aux2 = aux[6:] # can use 9 aux
-    def proc():
-        for i in range(6):
-            r, c = int(stars[i][0]), int(stars[i][1])
-            q1, q2 = perm[r*2], perm[r*2+1]
-            if c == 0:
-                qc.x(q1)
-                qc.x(q2)
-            elif c == 1:
-                qc.x(q1)
-            elif c == 2:
-                qc.x(q2)
-
-            qc.rccx(q1, q2, workspace[i])
-
-            if c == 0:
-                qc.x(q1)
-                qc.x(q2)
-            elif c == 1:
-                qc.x(q1)
-            elif c == 2:
-                qc.x(q2)
-
-    proc()
-    is_count4(qc, workspace, aux2[0], aux2[1:6], aux2[6])
-    qc.mct([addr[0], addr[1], addr[2], addr[3], aux2[0]], oracle, aux2[6:9], mode='basic')
-    inv_is_count4(qc, workspace, aux2[0],aux2[1:6], aux2[6])
-    proc()
+# write to addr == 1111
+def store_data(qc, addr, data, aux, board):
+    qc.mct(addr, aux[0], aux[1:], mode='basic')
+    if len(board[0]) != 0 and board[0][0] == 0:
+        qc.cx(aux[0], data[0])
+    if len(board[1]) != 0 and board[1][0] == 1:
+        qc.cx(aux[0], data[1])
+    for r in range(2, 4):
+        for c in board[r]:
+            if c == 2:
+                qc.cx(aux[0], data[(r-1)*2])
+            elif c == 3:
+                qc.cx(aux[0], data[(r-1)*2+1])
+    qc.mct(addr, aux[0], aux[1:], mode='basic')
 
 # memo: gray_code
 # 15, 14, 10, 11, 9, 8, 0, 1, 3, 2, 6, 7, 5, 4, 12, 13
-def phase_oracle(qc, addr, perm, oracle, aux, problem_set):
-    create_perm(qc, perm, oracle, aux)
-
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b1111])
+def store_stars(qc, addr, data, aux, problem_set):
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b1111]))
     qc.x(addr[3])
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b1110])
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b1110]))
     qc.x(addr[1])
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b1010])
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b1010]))
     qc.x(addr[3])
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b1011])
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b1011]))
     qc.x(addr[2])
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b1001])
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b1001]))
     qc.x(addr[3])
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b1000])
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b1000]))
     qc.x(addr[0])
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b0000])
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b0000]))
     qc.x(addr[3])
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b0001])
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b0001]))
     qc.x(addr[2])
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b0011])
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b0011]))
     qc.x(addr[3])
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b0010])
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b0010]))
     qc.x(addr[1])
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b0110])
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b0110]))
     qc.x(addr[3])
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b0111])
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b0111]))
     qc.x(addr[2])
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b0101])
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b0101]))
     qc.x(addr[3])
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b0100])
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b0100]))
     qc.x(addr[0])
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b1100])
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b1100]))
     qc.x(addr[3])
-    check_one_board(qc, addr, perm, oracle, aux, problem_set[0b1101])
+    store_data(qc, addr, data, aux, preprocess(problem_set[0b1101]))
     qc.x(addr[2])
 
-    # inversion
-    inv_create_perm(qc, perm, oracle, aux)
+
+
+def phase_oracle(qc, addr, data, perm, oracle, aux, problem_set):
+    store_stars(qc, addr, data, aux, problem_set)
+
+    qc.ccx(data[0], data[1], aux[0])
+    qc.x(perm[0])
+    qc.mct([perm[0], data[2], perm[1], data[5]], aux[1], aux[3:], mode='basic')
+    qc.x(perm)
+    qc.mct([perm[0], data[3], perm[1], data[4]], aux[2], aux[3:], mode='basic')
+    qor(qc, aux[1], aux[2], aux[3])
+
+    qc.ccx(aux[0], aux[3], oracle)
+
+    inv_qor(qc, aux[1], aux[2], aux[3])
+    qc.mct([perm[0], data[3], perm[1], data[4]], aux[2], aux[3:], mode='basic')
+    qc.x(perm)
+    qc.mct([perm[0], data[2], perm[1], data[5]], aux[1], aux[3:], mode='basic')
+    qc.x(perm[0])
+    qc.ccx(data[0], data[1], aux[0])
+
+    store_stars(qc, addr, data, aux, problem_set)
 
 
 def diffusion(qc, addr, perm, aux):
-    tmp = [None] * 12
-    for i in range(4):
-        tmp[i] = addr[i]
-    for i in range(8):
-        tmp[i + 4] = perm[i]
+    tmp = [addr[0], addr[1], addr[2], addr[3], perm[0], perm[1]]
     qc.h(tmp)
     qc.x(tmp)
-    qc.h(tmp[11])
-    qc.mct(tmp[0:11], tmp[11], aux, mode='basic')
-    qc.h(tmp[11])
+    qc.h(tmp[5])
+    qc.mct(tmp[0:5], tmp[5], aux, mode='basic')
+    qc.h(tmp[5])
     qc.x(tmp)
     qc.h(tmp)
 
 
 def week3_ans_func(problem_set):
     # TODO: test by changing the order
-    problem_set = \
-        [[['0', '2'], ['1', '0'], ['1', '2'], ['1', '3'], ['2', '0'], ['3', '3']],
-        [['0', '0'], ['0', '1'], ['1', '2'], ['2', '2'], ['3', '0'], ['3', '3']],
-        [['0', '0'], ['1', '1'], ['1', '3'], ['2', '0'], ['3', '2'], ['3', '3']],
-        [['0', '0'], ['0', '1'], ['1', '1'], ['1', '3'], ['3', '2'], ['3', '3']],
-        [['0', '2'], ['1', '0'], ['1', '3'], ['2', '0'], ['3', '2'], ['3', '3']],
-        [['1', '1'], ['1', '2'], ['2', '0'], ['2', '1'], ['3', '1'], ['3', '3']],
-        [['0', '2'], ['0', '3'], ['1', '2'], ['2', '0'], ['2', '1'], ['3', '3']],
-        [['0', '0'], ['0', '3'], ['1', '2'], ['2', '2'], ['2', '3'], ['3', '0']],
-        [['0', '3'], ['1', '1'], ['1', '2'], ['2', '0'], ['2', '1'], ['3', '3']],
-        [['0', '0'], ['0', '1'], ['1', '3'], ['2', '1'], ['2', '3'], ['3', '0']],
-        [['0', '1'], ['0', '3'], ['1', '2'], ['1', '3'], ['2', '0'], ['3', '2']],
-        [['0', '0'], ['1', '3'], ['2', '0'], ['2', '1'], ['2', '3'], ['3', '1']],
-        [['0', '1'], ['0', '2'], ['1', '0'], ['1', '2'], ['2', '2'], ['2', '3']],
-        [['0', '3'], ['1', '0'], ['1', '3'], ['2', '1'], ['2', '2'], ['3', '0']],
-        [['0', '2'], ['0', '3'], ['1', '2'], ['2', '3'], ['3', '0'], ['3', '1']],
-        [['0', '1'], ['1', '0'], ['1', '2'], ['2', '2'], ['3', '0'], ['3', '1']]]
-    problem_set[0], problem_set[10] = problem_set[10], problem_set[0]
+    #problem_set = \
+    #    [
+    #    [['0', '2'], ['1', '0'], ['1', '2'], ['1', '3'], ['2', '0'], ['3', '3']],
+    #    [['0', '1'], ['0', '3'], ['1', '2'], ['1', '3'], ['2', '0'], ['3', '2']], # answer
+    #    [['0', '0'], ['0', '1'], ['1', '2'], ['2', '2'], ['3', '0'], ['3', '3']],
+    #    [['0', '0'], ['1', '1'], ['1', '3'], ['2', '0'], ['3', '2'], ['3', '3']],
+    #    [['0', '0'], ['0', '1'], ['1', '1'], ['1', '3'], ['3', '2'], ['3', '3']],
+    #    [['0', '2'], ['1', '0'], ['1', '3'], ['2', '0'], ['3', '2'], ['3', '3']],
+    #    [['1', '1'], ['1', '2'], ['2', '0'], ['2', '1'], ['3', '1'], ['3', '3']],
+    #    [['0', '2'], ['0', '3'], ['1', '2'], ['2', '0'], ['2', '1'], ['3', '3']],
+    #    [['0', '0'], ['0', '3'], ['1', '2'], ['2', '2'], ['2', '3'], ['3', '0']],
+    #    [['0', '3'], ['1', '1'], ['1', '2'], ['2', '0'], ['2', '1'], ['3', '3']],
+    #    [['0', '0'], ['0', '1'], ['1', '3'], ['2', '1'], ['2', '3'], ['3', '0']],
+    #    [['0', '0'], ['1', '3'], ['2', '0'], ['2', '1'], ['2', '3'], ['3', '1']],
+    #    [['0', '1'], ['0', '2'], ['1', '0'], ['1', '2'], ['2', '2'], ['2', '3']],
+    #    [['0', '3'], ['1', '0'], ['1', '3'], ['2', '1'], ['2', '2'], ['3', '0']],
+    #    [['0', '2'], ['0', '3'], ['1', '2'], ['2', '3'], ['3', '0'], ['3', '1']],
+    #    [['0', '1'], ['1', '0'], ['1', '2'], ['2', '2'], ['3', '0'], ['3', '1']],
+    #    ]
 
     address = QuantumRegister(4)
-    perm = QuantumRegister(8) # row and col
+    data = QuantumRegister(6) # board data
+    perm = QuantumRegister(2) # right below
     oracle = QuantumRegister(1)
     aux = QuantumRegister(15)
     solution = ClassicalRegister(4)
-    #solution = ClassicalRegister(8) # perm
-    #solution = ClassicalRegister(2) # adder
-    #solution = ClassicalRegister(5) # is_count4, inv_is_count4
-    #solution = ClassicalRegister(9) # check_one_board
-    #solution = ClassicalRegister(12) # oracle
-    qc = QuantumCircuit(address, perm, oracle, aux, solution)
+    #solution = ClassicalRegister(7)
+    qc = QuantumCircuit(address, data, perm, oracle, aux, solution)
 
     # answer -------------------------------------------------------------------
     # initialize
+    # create |01> + |10>
+    qc.h(perm[0])
+    qc.cx(perm[0], perm[1])
+    qc.x(perm[0])
     qc.h(address)
-    qc.h(perm)
     qc.x(oracle)
     qc.h(oracle)
 
     for i in range(1):
-        phase_oracle(qc, address, perm, oracle, aux, problem_set)
+        phase_oracle(qc, address, data, perm, oracle, aux, problem_set)
         diffusion(qc, address, perm, aux)
 
     qc.measure(address, solution)
+    qc.reverse_bits()
     # answer: addr == 10 (0b1010)
 
-    # test perm ----------------------------------------------------------------
-    #qc.h(perm)
-    #qc.x(oracle)
-    #qc.h(oracle)
-
-    #create_perm(qc, perm, oracle[0], aux)
-
-    #qc.measure(perm, solution)
-
-    # test adder3 --------------------------------------------------------------
-    #qc.h(aux[0:2])
-    #qc.x(aux[2])
-    #adder2(qc, aux[0:2], aux[2], aux[3])
-    #qc.measure(aux[0:2], solution[0:2])
-
-    # test is_count4 -----------------------------------------------------------
-    #qc.h(perm[0:4])
-    #is_count4(qc, perm[0:6], oracle[0], aux[0:4], aux[4:])
-    #qc.measure(perm[0:4], solution[0:4])
-    #qc.measure(oracle[0], solution[4])
-
-    # test inv_is_count4 -------------------------------------------------------
-    #qc.h(perm[0:4])
-    #is_count4(qc, perm[0:6], oracle[0], aux)
-    #inv_is_count4(qc, perm[0:6], oracle[0], aux)
-    #qc.measure(perm[0:4], solution[0:4])
-    #qc.measure(oracle[0], solution[4])
-
-    # test check_one_board -----------------------------------------------------
-    #qc.h(perm)
-    #qc.x(address)
-    #board = problem_set[0]
-    #check_one_board(qc, address, perm, oracle, aux, board)
-    #qc.measure(perm, solution[0:8])
-    #qc.measure(oracle, solution[8])
-
-    # test oracle --------------------------------------------------------------
-    #qc.h(perm)
+    # test oracle
+    #qc.h(perm[0])
+    #qc.cx(perm[0], perm[1])
+    #qc.x(perm[0])
     #qc.h(address)
-    #qc.x(oracle)
-    #qc.h(oracle)
 
-    #for i in range(2):
-    #    create_perm(qc, perm, oracle, aux)
-    #    check_one_board(qc, address, perm, oracle, aux, problem_set[10])
-    #    inv_create_perm(qc, perm, oracle, aux)
+    #store_data(qc, address, data, aux, preprocess(problem_set[1]))
+    #qc.x(perm[0])
+    #qc.mct([perm[0], data[2], perm[1], data[5]], aux[0])
+    #qc.x(perm)
+    #qc.mct([perm[0], data[3], perm[1], data[4]], aux[1])
+    #qor(qc, aux[0], aux[1], oracle)
 
-    #    diffusion(qc, address, perm, aux)
-
-
-    #qc.h(oracle)
-    #qc.x(oracle)
-    ##qc.measure(perm, solution[0:8])
-    #qc.measure(address, solution[8:12])
+    #qc.measure(data, solution[0:6])
+    #qc.measure(oracle, solution[6])
 
     return qc
